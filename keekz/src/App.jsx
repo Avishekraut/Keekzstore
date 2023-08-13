f601ca92fa7f4ffa67ceb585b8236cb15d1cf0bc
Avishek Raut <avishekraut123@gmail.com>
2023-08-14 00:53:20 +0545

    updated App component

--- keekz/src/App.jsx
+++ keekz/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './component/Header'
+import Hero from './component/Hero'
 
 const App = () => {
   return (
     <>
       <Header/>
+      <Hero/>
     </>
   )
 }
