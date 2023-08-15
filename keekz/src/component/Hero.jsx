553bb2fde16d3dd9d37d0128dd0303eeb7861ba4
Avishek Raut <avishekraut123@gmail.com>
2023-08-15 13:40:19 +0545

    removed hero css

--- keekz/src/component/Hero.jsx
+++ keekz/src/component/Hero.jsx
@@ -5,19 +5,7 @@ import img3 from "../images/img3.png";
 const Hero = () => {
   return (
     <section>
-    <div className="card-container">
-      <div className="left-column">
-        <img src={img1} alt=""></img>
-      </div>
-      <div className="right-column">
-        <div className="card1">
-          <img src={img2} alt=""></img>
-        </div>
-        <div className="card2">
-          <img src={img3} alt=""></img>
-        </div>
-      </div>
-    </div>
+    
     </section>
   );
 };
