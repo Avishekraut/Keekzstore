4b39070fc6d90e13dfc9948301af8c205165e767
Niiischal <nischal.khatiwada745@gmail.com>
2023-08-15 13:08:08 +0545

    created title component and added content for the heading to it

--- keekz/src/App.jsx
+++ keekz/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './component/Header'
 import Hero from './component/Hero'
+import Title from './component/Title'
 
 const App = () => {
   return (
     <>
       <Header/>
-      <Hero/>
+      {/* <Hero/> */}
+      <Title/>
     </>
   )
 }
---
+++ keekz/src/component/Title.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/style.css'
+
+const Title = () => {
+  return (
+    <section>
+        <div className="title">
+            <h1>Don't miss out new drops</h1>
+        </div>
+        <div className="button">
+            
+        </div>
+    </section>
+  )
+}
+
+export default Title
