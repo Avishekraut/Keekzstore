6d7d9774d81094354198a00209231ffc34bd7eb8
Avishek Raut <avishekraut123@gmail.com>
2023-08-15 15:06:31 +0545

    Hero component update for Carousel

--- keekz/src/component/Hero.jsx
+++ keekz/src/component/Hero.jsx
@@ -1,9 +1,22 @@
+import Carousel from '../component/Carousel'
+import carousel_img1 from '../images/carousel-img1.jpg'
+import carousel_img2 from '../images/carousel-img2.jpg'
+import carousel_img3 from '../images/carousel-img3.jpg'
 
+const slides = [
+  carousel_img1,
+  carousel_img2,
+  carousel_img3
+]
 
 const Hero = () => {
   return (
     <section>
-    
+      <Carousel autoSlide={true}>
+        {slides.map((item) => (
+          <img src={item} alt=''/>
+        ))}
+      </Carousel>
     </section>
   );
 };
