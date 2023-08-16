7b1afd32e4a43df8d02212a219ef9724dc2f4cce
Niiischal <nischal.khatiwada745@gmail.com>
2023-08-16 13:37:10 +0545

    removed the unwanted div in CategoriesCard component and added the third card for category

--- keekz/src/component/CategoriesCard.jsx
+++ keekz/src/component/CategoriesCard.jsx
@@ -1,15 +1,26 @@
-
+import categoryimg1 from '../images/category-card-img1.png'
+import categoryimg2 from '../images/category-card-img2.png'
 const CategoriesCard = () => {
   return (
     <div className="first-card-component">
       <div className="first-card-image">
-        <h2>Lifestyle <br />shoes</h2>
-      </div>
-      <div className="second-card-component">
-        <div className="second-card-image">
-        <h2>Basketball <br />shoes</h2>
+        <img src={categoryimg1} alt="" />
+        <div className="first-card-text">
+        <h2>Lifestyle shoes</h2>
         </div>
       </div>
+        <div className="second-card-image">
+        <img src={categoryimg2} alt="" />
+          <div className="second-card-text">
+          <h2>Basketball shoes</h2>
+          </div>
+        </div>
+        <div className="third-card-image">
+        <img src={categoryimg2} alt="" />
+          <div className="third-card-text">
+          <h2>Basketball shoes</h2>
+          </div>
+        </div>
     </div>
   )
 }
