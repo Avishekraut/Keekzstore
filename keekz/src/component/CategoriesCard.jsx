3fd42d7713b13674946f160aa8bacfe60ff09d41
Niiischal <nischal.khatiwada745@gmail.com>
2023-08-16 13:40:05 +0545

    categorically managed the css for the whole category component

--- keekz/src/component/CategoriesCard.jsx
+++ keekz/src/component/CategoriesCard.jsx
@@ -2,7 +2,7 @@ import categoryimg1 from '../images/category-card-img1.png'
 import categoryimg2 from '../images/category-card-img2.png'
 const CategoriesCard = () => {
   return (
-    <div className="first-card-component">
+    <div className="category-card-component">
       <div className="first-card-image">
         <img src={categoryimg1} alt="" />
         <div className="first-card-text">
