8e20083c3576f8bd6054f2d71efcc55cfa72d231
Niiischal <nischal.khatiwada745@gmail.com>
2023-08-16 14:40:18 +0545

    categories component finished with appropriate css

--- keekz/src/component/CategoriesCard.jsx
+++ keekz/src/component/CategoriesCard.jsx
@@ -1,24 +1,25 @@
-import categoryimg1 from '../images/category-card-img1.png'
-import categoryimg2 from '../images/category-card-img2.png'
+import categoryimg1 from '../images/category-card-img1.jpg'
+import categoryimg2 from '../images/category-card-img2.jpg'
+import categoryimg3 from '../images/category-card-img3.jpg'
 const CategoriesCard = () => {
   return (
     <div className="category-card-component">
       <div className="first-card-image">
         <img src={categoryimg1} alt="" />
         <div className="first-card-text">
-        <h2>Lifestyle shoes</h2>
+        <h2>Lifestyle</h2>
         </div>
       </div>
         <div className="second-card-image">
         <img src={categoryimg2} alt="" />
           <div className="second-card-text">
-          <h2>Basketball shoes</h2>
+          <h2>Basketball</h2>
           </div>
         </div>
         <div className="third-card-image">
-        <img src={categoryimg2} alt="" />
+        <img src={categoryimg3} alt="" />
           <div className="third-card-text">
-          <h2>Basketball shoes</h2>
+          <h2>Slides</h2>
           </div>
         </div>
     </div>
