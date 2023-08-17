91d199183ec133c6467e015b5598686a69ca0b24
Avishek Raut <avishekraut123@gmail.com>
2023-08-17 17:02:22 +0545

    updated data.js

---
+++ keekz/src/data.js
@@ -0,0 +1,49 @@
+const data = [
+    {
+      "id": 1,
+      "name": "Air Jordan 1 High Electro Orange",
+      "brand": "Nike",
+      "price": 129.99,
+      "color": "White/Black",
+      "image": "electroorange.jpg",
+      "description": "The Nike Air Max 90 offers...",
+      "availability": true,
+      "sizes": ["US 7", "US 8", "US 9"]
+    },
+    {
+      "id": 2,
+      "name": "AJ 1 Low X Travis Scott Olive",
+      "brand": "Adidas",
+      "price": 149.99,
+      "color": "Black",
+      "image": "olive.jpg",
+      "description": "Experience ultimate comfort...",
+      "availability": true,
+      "sizes": ["US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 3,
+      "name": "J1 Retro OG SP Black Phantom",
+      "brand": "Puma",
+      "price": 89.99,
+      "color": "White/Pink",
+      "image": "phantom.jpg",
+      "description": "The Puma Cali delivers...",
+      "availability": false,
+      "sizes": ["US 6", "US 7", "US 8"]
+    },
+    {
+      "id": 3,
+      "name": "Jordan 1 RETRO OG HIGH MOCHA",
+      "brand": "Puma",
+      "price": 89.99,
+      "color": "White/Pink",
+      "image": "Mocha-High.jpg",
+      "description": "The Puma Cali delivers...",
+      "availability": false,
+      "sizes": ["US 6", "US 7", "US 8"]
+    }
+  ]
+;
+
+export default data;
