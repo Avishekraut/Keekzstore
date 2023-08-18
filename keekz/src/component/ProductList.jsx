fd3f81f8d6bc463305e27293e49d91f2237e9ae7
Avishek Raut <avishekraut123@gmail.com>
2023-08-18 14:18:52 +0545

    price element added in product list comp

--- keekz/src/component/ProductList.jsx
+++ keekz/src/component/ProductList.jsx
@@ -6,6 +6,7 @@ function ProductList(props) {
     <div className='product-details'>
        <img src={require(`../images/${props.value.image}`)} alt=''></img>
        <h2 className='product-details-title'>{props.value.name}</h2>
+       <h2 className='product-details-price'>{`$ ${props.value.price}`}</h2>
        <AddToCartBtn />
     </div>
   );
