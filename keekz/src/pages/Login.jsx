a8f1c7a313d9acac84b9a26a817ab9d8dbe82986
Niiischal <nischal.khatiwada745@gmail.com>
2023-08-27 16:51:31 +0545

    added the TextInputs component for the inputs

--- keekz/src/pages/Login.jsx
+++ keekz/src/pages/Login.jsx
@@ -1,9 +1,20 @@
 import image from "../images/login-signup.png";
+import TextInputs from "../component/TextInputs";
 const Login = () => {
   return (
     <div className="login-container">
         <div className="login-image">
         <img src={image} alt="" />
+        </div>
+        <div className="login-content">
+        <div className="top-title">
+          <h1>Login</h1>
+          <p>Sign in with</p>
+        </div>
+        <div className="login-details">
+          <TextInputs type="email" placeholder="Email" />
+          <TextInputs type="password" placeholder="Password" />
+        </div>
         </div>
     </div>
   )
